54a94601d2f2d29b5b018c347d5f8128f0b2bd82
Mateusz Jankowski <matijank10@gmail.com>
2021-11-09 15:15:30 +0100

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "should get correct title" do
+    get signup_path
+    assert_response :success
+    assert_select 'title', full_title("Sign up")
+  end
+end
